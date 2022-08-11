f3d02e4ed707a405bae49a34fcc341a6d994e96c
danielxgoldin <104451480+danielxgoldin@users.noreply.github.com>
2022-08-11 10:08:53 -0700

    fixing more

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const apiRoutes = require('./routes/apiroutes');
-const htmlRoutes = require('./routes/htmlroutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({
     extended: true
@@ -12,8 +12,8 @@ app.use(express.urlencoded({
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use('/api', apiroutes);
-app.use('/', htmlroutes);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {
